ddef6adba214f29a37de9312d7c61b4630857959
Rz-Adamer <adamerplaying@gmail.com>
2026-04-29 20:44:37 -0500

    Vista lista de empresas
    Se añadio las vistas de gestion de empresas y en el index se añadio a empresa a index para su visualizacion.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import Login from './pages/auth/Login';
+import EmpresaList from './pages/empresa/EmpresaList';
+import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
-    <Login />
+    <EmpresaList />
+    </BrowserRouter>
   </React.StrictMode>
 );
